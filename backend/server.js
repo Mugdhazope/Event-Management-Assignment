726dfad2978e1bb41379be739c6ff9c2c93d90df
MugdhaZope <mugdhadzope@gmail.com>
2025-12-08 00:17:31 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,7 @@ const connectDB = async () => {
 connectDB();
 
 const PORT = process.env.PORT || 5001;
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-    });
-  }
+
   
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
