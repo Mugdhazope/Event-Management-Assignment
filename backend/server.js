631d1b61a30b4a8e2eb6efac36333000a6bbed5e
MugdhaZope <mugdhadzope@gmail.com>
2025-12-08 02:55:02 +0530

    prod-backend-fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://event-management-assignment-seven.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
